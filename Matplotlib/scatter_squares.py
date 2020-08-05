0f1cf99b9bfc5afb5178026d7324f6c25380de02
sanhuanghuang <13551296510@163.com>
2020-08-05 11:13:08 +0800

    代码整理

--- Matplotlib/scatter_squares.py
+++ Matplotlib/scatter_squares.py
@@ -1,8 +1,7 @@
-"""
-画离散的点
+from matplotlib import pyplot as plt
+
 x_values = list(range(1, 1001))
 y_value = [x ** 2 for x in x_values]
 plt.scatter(x_values, y_value, s=40, c=y_value, cmap=plt.cm.Blues, edgecolors='none')
 plt.axis([0, 1100, 0, 1100000])
 plt.show()
-"""
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # PythonDemo
 基于Python的数据可视化demo
 - 基于matplotlib, Pygal的数据可视化
+- CSV文件数据处理
